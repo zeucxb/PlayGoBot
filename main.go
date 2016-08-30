1a4cd326717bfe003b10011c417675ff07501165
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2016-08-30 03:06:58 -0300

    Bug Fix

--- main.go
+++ main.go
@@ -2,34 +2,26 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/sclevine/agouti"
 )
 
 func main() {
-	var digited, imports string
+	var videoURL string
+	var viewsQTD int
+	// var videoTime int
 
-	fmt.Println("What you want to import?")
-	fmt.Scan(&imports)
+	fmt.Println("What is the url of your video?")
+	fmt.Scan(&videoURL)
 
-	fmt.Println("Show me the code :)")
-	fmt.Scan(&digited)
+	fmt.Println("How mane views do you like?")
+	fmt.Scan(&viewsQTD)
 
-	ims := strings.Split(imports, ",")
+	// fmt.Println("What is the length of your video? (ex. 1.30 = 01:30 min)")
+	// fmt.Scan(&videoTime)
 
-	imports = "import("
-
-	for i := range ims {
-		imports += fmt.Sprintf("\"%s\"\n", ims[i])
-	}
-
-	imports += ")"
-
-	digited = fmt.Sprintf("package main\n%s\nfunc main() { %s }", imports, digited)
-
-	fmt.Println("========== RESPONSE ==========")
+	// videoTime *= 1000
 
 	driver := agouti.PhantomJS()
 	if err := driver.Start(); err != nil {
@@ -40,38 +32,23 @@ func main() {
 		fmt.Println("Failed to open page:", err)
 	}
 
-	if err := page.Navigate("https://play.golang.org/p/m-70CPrfAC"); err != nil {
-		fmt.Println("Failed to navigate:", err)
+	for viewsQTD > 0 {
+		if err := page.Navigate(videoURL); err != nil {
+			fmt.Println("Failed to navigate:", err)
+		}
+
+		time.Sleep(1000 * time.Millisecond)
+
+		views, _ := page.Find(".watch-view-count").Text()
+
+		fmt.Println("VIEWS:", views)
+
+		viewsQTD--
+
+		fmt.Printf("Views remaining %d\n", viewsQTD)
 	}
 
-	_, err = page.URL()
-	if err != nil {
-		fmt.Println("Failed to get page URL:", err)
+	if err := driver.Stop(); err != nil {
+		fmt.Println("Failed to close pages and stop WebDriver:", err)
 	}
-
-	page.Find("#code").SendKeys(digited)
-
-	page.Find("#format").Click()
-
-	page.Find("#run").Click()
-
-	time.Sleep(1000 * time.Millisecond)
-
-	loginPrompt, err := page.Find("#output").Text()
-	if err != nil {
-		fmt.Println("Failed to get login prompt text:", err)
-	}
-
-	fmt.Println(loginPrompt)
-
-	// loginPrompt, err = page.Find("#code").Text()
-	// if err != nil {
-	// 	fmt.Println("Failed to get login prompt text:", err)
-	// }
-
-	// fmt.Println("code:", loginPrompt)
-
-	// if err := driver.Stop(); err != nil {
-	// 	fmt.Println("Failed to close pages and stop WebDriver:", err)
-	// }
 }
